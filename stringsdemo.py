ed3fe354acd8eb6d11d0eb9ead1c264a24ba93f5
pol4 <vpol4mail@gmail.com>
2023-11-03 23:23:32 +0300

    Add files via upload

---
+++ stringsdemo.py
@@ -0,0 +1,18 @@
+def input_digital():
+    x_orig = input()
+    if x_orig.isdecimal():
+        return int(x_orig)
+    else:
+        return False
+
+def main():
+    print("Введите число:")
+
+    while (x := input_digital()) == False:
+        print("Фигню ввёл. Попробуй ещё раз.")
+
+    print("Введено число:", x)
+
+
+if __name__ == "__main__":
+    main()
---
+++ stringsdemo_2.py
@@ -0,0 +1,20 @@
+def input_digital():
+    try:
+        x_orig = input()
+        x = int(x_orig)
+        return x
+    except ValueError:
+        return False
+
+
+def main():
+    print("Введите число:")
+
+    while not (x := input_digital()):
+        print("Фигню ввёл. Попробуй ещё раз")
+
+    print("Введено число: ", x, "\nТип данных: ", type(x))
+
+
+if __name__ == '__main__':
+    main()
